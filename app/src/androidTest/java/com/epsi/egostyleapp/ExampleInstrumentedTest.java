e4eaf08c0efc21d39d05a9cec9351a1aa7ccd8d5
EcremE <elodie-ecrement@live.fr>
2020-02-12 11:48:07 +0100

    Create app skeleton

--- app/src/androidTest/java/com/epsi/egostyleapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/epsi/egostyleapp/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.epsi.egostyleapp;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
