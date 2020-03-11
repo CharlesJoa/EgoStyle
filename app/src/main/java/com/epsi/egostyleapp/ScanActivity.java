780a76acdbfacd9bec430a8f6691162c38313586
EcremE <elodie-ecrement@live.fr>
2020-03-11 09:53:18 +0100

    Add QRcode scan

--- app/src/main/java/com/epsi/egostyleapp/ScanActivity.java
+++ app/src/main/java/com/epsi/egostyleapp/ScanActivity.java
@@ -1,20 +1,30 @@
 package com.epsi.egostyleapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class ScanActivity extends AppCompatActivity {
+    private Button btn_scan; //Ajout partie scan
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        //Partie navigation
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.scanner);
 
@@ -40,5 +50,40 @@ public class ScanActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        //Partie scan
+        btn_scan = (Button) findViewById(R.id.btn_scan);
+        final Activity activity = this;
+        btn_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null){
+            if (result.getContents() == null){
+                Toast.makeText(this,"Impossible de scanner", Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(this,result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+
     }
 }
